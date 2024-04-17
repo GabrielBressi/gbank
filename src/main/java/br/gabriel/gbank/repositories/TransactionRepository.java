b9db8a637c3327546202f40c9ab7e9630965d9b9
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-17 18:38:41 -0300

    :sparkles: feat: created repositories for account, transaction and user

---
+++ src/main/java/br/gabriel/gbank/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package br.gabriel.gbank.repositories;
+
+import br.gabriel.gbank.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
---
+++ src/main/java/br/gabriel/gbank/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.gabriel.gbank.repositories;
+
+import br.gabriel.gbank.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findUserByEmail(String email);
+}
