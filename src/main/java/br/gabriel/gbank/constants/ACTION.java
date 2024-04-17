7f1e8e66da1166695bfc258f3b7891a06aa6df66
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-17 18:33:52 -0300

    feat: created constants

---
+++ src/main/java/br/gabriel/gbank/constants/ACTION.java
@@ -0,0 +1,7 @@
+package br.gabriel.gbank.constants;
+
+public enum ACTION {
+
+    DEPOSIT,
+    WITHDRAW
+}
---
+++ src/main/java/br/gabriel/gbank/constants/GbankConstants.java
@@ -0,0 +1,7 @@
+package br.gabriel.gbank.constants;
+
+import java.util.regex.Pattern;
+
+public class GbankConstants {
+    public static final String USER = "USER";
+}
